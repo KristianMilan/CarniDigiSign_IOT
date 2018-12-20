918b7225f1cbd1f442dd235df9c52142c2be5a95
Chris Grabosky <chris@grabosky.net>
2018-12-20 10:48:59 -0500

    updated with stitch-cli export instead

--- Stitch/new.js
+++
@@ -1,8 +0,0 @@
-
-// Try running in the console below.
-  
-exports = async function(payload,response) {
-    var sign = payload.body.text();
-    var conn = context.services.get("mongodb-atlas").db("digisign").collection("screens");
-    await conn.insertOne(JSON.parse(sign));
-  };
---
+++ Stitch/services/screens/incoming_webhooks/feeds/source.js
@@ -0,0 +1,14 @@
+// Try running in the console below.
+  
+exports = async function(payload,response) {
+  var feed = payload.query.feed || '';
+  var conn = context.services.get("mongodb-atlas").db("digisign").collection("screens");
+  var r = '';
+  var docs = await conn.find({"feed":feed}).toArray()
+  r = JSON.stringify(docs);
+  
+  //console.log(r);
+  response.setHeader("Content-Type", "application/json");
+  response.setBody(r);
+
+};
