74da423546b1ee9af072bfe2eed1b3afb4298b81
Chris Grabosky <chris@grabosky.net>
2019-01-11 20:34:19 -0500

    Fixing issue i have locally thats probably bad for the community

--- Stitch/services/screens/incoming_webhooks/feeds/source.js
+++ Stitch/services/screens/incoming_webhooks/feeds/source.js
@@ -4,7 +4,7 @@ exports = async function(payload,response) {
   var feed = payload.query.feed || '';
   var conn = context.services.get("mongodb-atlas").db("digisign").collection("screens");
   var r = '';
-  var docs = await conn.find({"feed":feed}).toArray()
+  var docs = await conn.find({"$or": [{"feed":feed}, {"feed":'all'}]}).toArray()
   r = JSON.stringify(docs);
   
   //console.log(r);
