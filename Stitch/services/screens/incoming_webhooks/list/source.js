bf7ca05b34ecb165792803ea5b294d5ecac0249b
Chris Grabosky <chris@grabosky.net>
2019-01-13 14:11:12 -0500

    pretty it up

--- Stitch/services/screens/incoming_webhooks/list/source.js
+++ Stitch/services/screens/incoming_webhooks/list/source.js
@@ -8,9 +8,9 @@ exports = async function(payload,response) {
   
   r = r + "<html><head><title>Device Inventory Details</title></head>";
   r = r +'<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">';
-  r = r + '<style>td,th { padding:5px; } .form-signin { max-width:50% !important; text-align:center; width:50%;margin-left:25%; } body { padding-top: 40px;  padding-bottom: 40px;  background-color: #f5f5f5; height:100%; width:100%;} </style>';
-  r = r + "<body><table class='form-signin table table-striped'>";
-  r = r + "<thead class='thead-dark'><tr><th>MAC</th><th>Token</th><th>Location</th><th>Link</th></tr></thead><tbody>";
+  r = r + '<style>td,th { padding:5px; } .form-signin { max-width:70% !important; text-align:center; width:70%;margin-left:15%; } body { padding-top: 40px;  padding-bottom: 40px;  background-color: #f5f5f5; height:100%; width:100%;} </style>';
+  r = r + "<body><h1 class='form-signin'>Registered Devices</h1><table class='form-signin table table-striped'>";
+  r = r + "<thead class='thead-dark'><tr><th>MAC</th><th>Token</th><th>Location</th><th>Last Seen</th><th>Link</th></tr></thead><tbody>";
   
   let docs = await conn.find().toArray();
   
@@ -20,6 +20,7 @@ exports = async function(payload,response) {
     r = r + "<td><code>"+docs[i].token|| ''+"</code></td>";
     r = r + "<td>"+docs[i].location|| ''+"</td>";
     var url = "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/digisign-ywoti/service/screens/incoming_webhook/details?token="+docs[i].token;
+    r = r + "<td>"+docs[i].lastseen|| ''+"</td>";
     r = r + "<td><a href='"+url+"'>Link</a></td>";
     r = r + "</tr>";
   };
