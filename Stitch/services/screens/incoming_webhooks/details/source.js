4ce66f7c3d2e4562fed03e4f3f604a7f74c369a5
Chris Grabosky <chris@grabosky.net>
2019-01-14 13:10:26 -0500

    nicer formatting of nulls

--- Stitch/services/screens/incoming_webhooks/details/source.js
+++ Stitch/services/screens/incoming_webhooks/details/source.js
@@ -1,4 +1,12 @@
 // Try running in the console below.
+
+function d(val) {
+  if ((val == "undefined") || (val == null) || (val == undefined)) {
+    return '';
+  } else {
+    return val;
+  }
+}
   
 exports = async function(payload,response) {
   var mac = payload.query.mac || '';
@@ -26,24 +34,27 @@ exports = async function(payload,response) {
     r = r + '<link rel="stylesheet" href="https://getbootstrap.com/docs/4.2/examples/sign-in/signin.css">';
     r = r + '<style>td,th { padding:5px; } .form-signin { max-width:700px !important; width: 700 !important%; } </style>';
     r = r + "<body style='text-center'>";
-    r = r + "<form method='get' action='"+postUrl+"' class='form-signin'>";
-    r = r + "<h3 class='form-signin'>Device Inventory Details</h3>"
+    r = r + "<form method='get' action='"+postUrl+"' class='form-signin' onsubmit='return validateForm()' id='f' name='f'>";
+    r = r + "<h3 class='form-signin'>Device Inventory Details</h3>";
+    r = r + "<small class='form-signin'><a href='"+rootUrl+"list'>Back to list</a></small>";
     r = r + "<table class='form-signin table table-striped'>";
     r = r + "<tr><th>ID</th><td><input type='text' name='id' class='form-control-plaintext' value='"+doc._id+"' readonly='true' /></td></tr>";
-    r = r + "<tr><th>MAC</th><td><input type='text' name='mac' class='form-control-plaintext' value='"+doc.mac+"' /></td></tr>";
+    r = r + "<tr><th>MAC</th><td><input type='text' name='mac' class='form-control-plaintext' value='"+d(doc.mac)+"' /></td></tr>";
     r = r + "<tr><th>Token</th><td><code>"+doc.token|| ''+"</code></td></tr>";
-    r = r + "<tr><th>Friendly Name</th><td><input type='text' name='name' class='form-control-plaintext' value='"+doc.name+"' /></td></tr>";
-    r = r + "<tr><th>Location</th><td><input type='text' name='location' class='form-control-plaintext' value='"+doc.location+"' /></td></tr>";
-    r = r + "<tr><th>Model</th><td><input type='text' name='model' class='form-control-plaintext' value='"+doc.model+"' /></td></tr>";
-    r = r + "<tr><th>Feed</th><td><input type='text' name='feed' class='form-control-plaintext' value='"+doc.feed+"' /></td></tr>";
-    r = r + "<tr><th>URL</th><td><input type='text' name='baseurl' class='form-control-plaintext' value='"+doc.baseurl+"' /></td></tr></tr>";
+    r = r + "<tr><th>Friendly Name</th><td><input type='text' name='name' class='form-control-plaintext' value='"+d(doc.name)+"' /></td></tr>";
+    r = r + "<tr><th>Location</th><td><input type='text' name='location' class='form-control-plaintext' value='"+d(doc.location)+"' /></td></tr>";
+    r = r + "<tr><th>Model</th><td><input type='text' name='model' class='form-control-plaintext' value='"+d(doc.model)+"' /></td></tr>";
+    r = r + "<tr><th>Feed</th><td><input type='text' name='feed' class='form-control-plaintext' value='"+d(doc.feed)+"' /></td></tr>";
+    r = r + "<tr><th>URL</th><td><input type='text' name='baseurl' class='form-control-plaintext' value='"+d(doc.baseurl)+"' /></td></tr></tr>";
     r = r + "<tr><th>Secret</th><td><input type='text' name='devicesecret' class='form-control-plaintext' value='' placeholder='REDACTED, REENTER!' /></td></tr>";
     r = r + "<tr><th>Initial Req Request</th><td>"+doc.firstseen|| ''+"</td></tr>";
     r = r + "<tr><th>Last Reg Reqeust</th><td>"+doc.lastseen|| ''+"</td></tr></tr>";
     r = r + "<tr><td colspan='2'><input type='password' class='form-control' id='secret' name='secret' placeholder='SECRET'></td></tr>";
     r = r + "<tr><td colspan='2' style='text-align:center;'><button type='submit' class='btn btn-primary'>Save</button></td></tr>";
     r = r + "<tr><th>QR Code</th><td style='background-color:#ffffff; text-align:center;'><img src='https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl="+encodeURI(thisUrl)+"'/></td>";
-    r = r + "</table></form></body></html>";
+    r = r + "</table></form>";
+    r = r + "<script>function validateForm() { if((document.forms['f']['devicesecret'].value.length>0)&&(document.forms['f']['secret'].value.length>0)) { return true; } else { alert('You must enter both secrets!'); return false;}}</script>"
+    r = r + "</body></html>";
   }
   else {
     r = r + "<html><body><h1>Device Inventory Details</h1><h2>Info not found!</h2></body></html>";
