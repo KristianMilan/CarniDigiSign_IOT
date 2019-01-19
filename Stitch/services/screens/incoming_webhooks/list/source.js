df9b9da4a64cfe3cd87734c1bd9c66d1315ced79
Chris Grabosky <chris@grabosky.net>
2019-01-19 15:06:47 -0500

    fix date formatting and turn link into icon

--- Stitch/services/screens/incoming_webhooks/list/source.js
+++ Stitch/services/screens/incoming_webhooks/list/source.js
@@ -1,5 +1,6 @@
 // Try running in the console below.
-function formatDate(date) {
+function formatDate(d) {
+  var date = new Date(d.getTime()-5*3600*1000); // bad utc calculations
   var hours = date.getHours();
   var minutes = date.getMinutes();
   var ampm = hours >= 12 ? 'pm' : 'am';
@@ -7,7 +8,7 @@ function formatDate(date) {
   hours = hours ? hours : 12; // the hour '0' should be '12'
   minutes = minutes < 10 ? '0'+minutes : minutes;
   var strTime = hours + ':' + minutes + ' ' + ampm;
-  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime + ' UTC';
+  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
 }
 
 function f(val,l) {
@@ -31,8 +32,9 @@ exports = async function(payload,response) {
   r = r + "<html><head><title>Device Inventory Details</title></head>";
   r = r +'<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">';
   r = r + '<style>td,th { padding:5px; } td{font-size:14px;} .form-signin { max-width:70% !important; text-align:center; width:70%;margin-left:15%; } body { padding-top: 40px;  padding-bottom: 40px;  background-color: #f5f5f5; height:100%; width:100%;} </style>';
+  r = r + '<script src="https://use.fontawesome.com/4132d47bbc.js"></script>';
   r = r + "<body><h1 class='form-signin'>Registered Devices</h1><table class='form-signin table table-striped'>";
-  r = r + "<thead class='thead-dark'><tr><th>MAC</th><th>IP</th><th>Token</th><th>Location</th><th>Last Seen</th><th>Link</th></tr></thead><tbody>";
+  r = r + "<thead class='thead-dark'><tr><th>MAC</th><th>IP</th><th>Token</th><th>Location</th><th>Last Seen</th><th><i class='fa fa-link' aria-hidden='true'></i></th></tr></thead><tbody>";
   
   let docs = await conn.find().toArray();
   
@@ -44,7 +46,7 @@ exports = async function(payload,response) {
     r = r + "<td>"+docs[i].location|| ''+"</td>";
     var url = "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/digisign-ywoti/service/screens/incoming_webhook/details?docid="+docs[i]._id;
     r = r + "<td>"+formatDate(docs[i].lastseen)|| ''+"</td>";
-    r = r + "<td><a href='"+url+"'>Link</a></td>";
+    r = r + "<td><a href='"+url+"'><i class='fa fa-link' aria-hidden='true'></i></a></td>";
     r = r + "</tr>";
   };
   
